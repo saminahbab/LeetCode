b64dca30868078c847a61d3b03c8769fd70d40ef
saminahbab <saminahbab0@gmail.com>
2019-08-14 15:19:33 +0100

    Completed Challenges

---
+++ src/main/java/validParentheses.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class validParentheses {
+
+
+    /*
+    Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+    An input string is valid if:
+
+    Open brackets must be closed by the same type of brackets.
+    Open brackets must be closed in the correct order.
+    Note that an empty string is also considered valid.
+
+
+     */
+    public static boolean isValid(String s) {
+        if(s.length() == 0) return true;
+        if((s.length() == 1 )|| s.startsWith("}|]|)")){ return false;}
+
+        HashMap<Character,Character> map = new HashMap<>();
+        map.put('}', '{');
+        map.put(')', '(');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        stack.push(s.charAt(0));
+
+        int pointer = 1;
+
+        while(pointer < s.length()){
+            if(stack.isEmpty()){stack.push(s.charAt(pointer)); pointer ++;}
+            else if(map.get(s.charAt(pointer)) == stack.peek()){stack.pop();pointer ++;}
+            else{stack.push(s.charAt(pointer)); pointer ++;}
+
+        }
+
+        return stack.isEmpty();
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.print(isValid("{}()[]"));
+    }
+}
